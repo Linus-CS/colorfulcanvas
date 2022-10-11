111e31143fd1f62c0662444169ff8bd230897a6d
Linus C. Schulze <linus.schulze@hsnet.de>
2022-10-11 18:41:56 +0200

    Changed json conversion

--- server/main.rs
+++ server/main.rs
@@ -1,16 +1,12 @@
 use axum::{
     http::StatusCode,
-    routing::{get, get_service, post},
+    routing::{get_service, post},
     Router,
 };
 use axum_server::tls_rustls::RustlsConfig;
-use mongodb::{
-    bson::{document, Document},
-    options::ClientOptions,
-    Client,
-};
+use mongodb::{bson, options::ClientOptions, Client};
 use serde::{Deserialize, Serialize};
-use serde_json::{Map, Number, Value};
+use serde_json::Value;
 use std::{io, net::SocketAddr};
 use tower_http::services::ServeDir;
 
@@ -35,23 +31,23 @@ async fn main() {
 }
 
 async fn save_canvas(body: String) {
-    let mut client_options = ClientOptions::parse("mongodb://localhost:27017")
+    let client_options = ClientOptions::parse("mongodb://localhost:27017")
         .await
         .unwrap();
+
     let grid: Grid = serde_json::from_str(&body).expect("Could not parse json to Grid struct.");
     println!("{:?}", grid);
 
     let client = Client::with_options(client_options).unwrap();
-
     let database = client.database("test");
+    let collections = database.collection("grids");
 
-    let collections = database.collection::<Document>("grids");
-    let mut document = Document::new();
-    document.insert("test", 10);
+    let document = bson::to_bson(&grid).unwrap();
+
     collections.insert_one(document, None).await.unwrap();
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct Grid {
     grid: Value,
